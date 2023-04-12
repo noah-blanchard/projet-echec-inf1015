2b82362159f3753b687c7817a90b3d8c715cf75e
BaiWuLi <libaiwu59678@gmail.com>
2023-04-12 14:31:35 -0400

    ...

--- ModelChecker.cpp
+++ ModelChecker.cpp
@@ -28,6 +28,7 @@ namespace logic {
 	}
 	
 	bool ModelChecker::validateMove(ModelSquare* square, ModelSquare* validMove) {
+		bool result = true;
 		ModelSquare* kingPosition = nullptr;
 		ModelSquare* tempSquare = new ModelSquare();
 		tempSquare->setPiece(validMove->getPiece());
@@ -35,33 +36,35 @@ namespace logic {
 		if (square == whiteKingSquare || square == blackKingSquare) {
 			kingPosition = validMove;
 		}
-		else{
-			if (square->getPiece()->isWhite())
-				kingPosition = whiteKingSquare;
-			else
-				kingPosition = blackKingSquare;
+		else if (square->getPiece()->isWhite()) {
+			kingPosition = whiteKingSquare;
+		}
+		else {
+			kingPosition = blackKingSquare;
 		}
 
-		square->setPiece(nullptr );
+		square->setPiece(nullptr);
 
 		for (int i = 0; i < 8; ++i) {
 			for (int j = 0; j < 8; ++j) {
-				
-				if (squares[i][j]->getPiece() != nullptr && squares[i][j]->getPiece()->isWhite() != validMove->getPiece()->isWhite()) {
-					std::vector<ModelSquare*> validMoves = squares[i][j]->getPiece()->getValidMoves(this, false);
-					if (std::find(validMoves.begin(), validMoves.end(), kingPosition) != validMoves.end()) {
-						square->setPiece(validMove->getPiece());
-						validMove->setPiece(tempSquare->getPiece());
-						//emit unallowedMoveSignal();
-						return false;
-					}
-				}
+				if (!(squares[i][j]->getPiece() != nullptr && squares[i][j]->getPiece()->isWhite() != validMove->getPiece()->isWhite()))
+					continue;
+
+				std::vector<ModelSquare*> validMoves = squares[i][j]->getPiece()->getValidMoves(this, false);
+
+				if (!(std::find(validMoves.begin(), validMoves.end(), kingPosition) != validMoves.end()))
+					continue;
+
+				result = false;
+				break;
+
 			}
 		}
+
 		square->setPiece(validMove->getPiece());
 		validMove->setPiece(tempSquare->getPiece());
 		delete tempSquare;
-		return true;		
+		return result;		
 	}
 	
 	ModelSquare* ModelChecker::getSelectedSquare() const
