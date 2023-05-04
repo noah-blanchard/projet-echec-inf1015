83adc0af0d1ced3c0c0e79bebee6070e36105d41
Noah <nb1@fastercom.ca>
2023-05-04 13:34:40 -0400

    fix test

--- Test.cpp
+++ Test.cpp
@@ -5,7 +5,13 @@ TEST(ModelKingPiece, Test_Max2_King) {
   logic::ModelKingPiece::resetInstanceCounter();
   logic::ModelKingPiece* king1 = new logic::ModelKingPiece(true);
   logic::ModelKingPiece* king2 = new logic::ModelKingPiece(true);
-  EXPECT_ANY_THROW(logic::ModelKingPiece* king3 = new logic::ModelKingPiece(true));
+  try {
+	  EXPECT_ANY_THROW(logic::ModelKingPiece * king3 = new logic::ModelKingPiece(true));
+  }
+  catch (std::exception& e) {
+	  std::cout << e.what() << std::endl;
+  }
   delete king1;
   delete king2;
+  logic::ModelKingPiece::resetInstanceCounter();
 }
