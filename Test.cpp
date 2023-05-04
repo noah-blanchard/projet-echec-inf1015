af3cde7141adc3bf2442d5f7594ae7afabe7b26c
Noah <nb1@fastercom.ca>
2023-05-04 15:28:43 -0400

    no possible move for king at the start

--- Test.cpp
+++ Test.cpp
@@ -4,7 +4,7 @@
 #include "ModelRook.h"
 #include "State.h"
 
-TEST(ModelKingPiece, Test_Max2_King) {
+TEST(ChessGame, Test_Max2_King) {
 	logic::ModelKingPiece::resetInstanceCounter();
 	logic::ModelKingPiece* king1 = new logic::ModelKingPiece(true);
 	logic::ModelKingPiece* king2 = new logic::ModelKingPiece(true);
@@ -17,9 +17,12 @@ TEST(ModelKingPiece, Test_Max2_King) {
 	delete king1;
 	delete king2;
 	logic::ModelKingPiece::resetInstanceCounter();
+
 }
 
-TEST(CheckMate, Test_Checkmate_1) {
+// test check mate situation 1
+
+TEST(ChessGame, Test_Checkmate_1) {
 	logic::ModelKingPiece::resetInstanceCounter();
 	logic::ModelChecker* checkerModel = logic::GameController::startGameFileLayout(new QFile("game_layouts/check_mate_situation1.txt"), false);
 	EXPECT_EQ(logic::GameController::isGameOver(), false);
@@ -29,5 +32,14 @@ TEST(CheckMate, Test_Checkmate_1) {
 	EXPECT_EQ(logic::GameController::getCurrentTurn()->isGameOver(checkerModel, isCheckMate), true);
 	logic::ModelKingPiece::resetInstanceCounter();
 	logic::ModelKingPiece::resetInstanceCounter();
+	delete checkerModel;
 }
-//TEST()
+
+// test no moves for king in the start of the game
+TEST(ChessGame, No_Possible_Moves) {
+	logic::ModelKingPiece::resetInstanceCounter();
+	logic::ModelChecker* checkerModel = logic::GameController::startGameFileLayout(new QFile("game_layouts/classic_game_layout.txt"), false);
+	EXPECT_EQ(checkerModel->getSquareAtPosition(7, 4)->getPiece()->getValidMoves2(checkerModel, false).size(), 0);
+	logic::ModelKingPiece::resetInstanceCounter();
+	delete checkerModel;
+}
